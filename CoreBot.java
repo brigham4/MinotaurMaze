a7da85a9ad83a19249c94ea274a3b1ed3ea14119
brigham4 <eric.brigham15@ncf.edu>
2017-11-08 14:26:08 -0500

    making core bot class as well as greed team class

---
+++ CoreBot.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class CoreBot implements Robot{
+    GreedTeam myTeam = new GreedTeam();
+    public ModelType getModel(){
+        //this sets the type of CoreBot to be CoreBot (seems redundant?)
+        ModelType core = new ModelType();
+        core = ModelType.CoreBot;
+        return core;
+    }
+    public int getID(){
+        //Here I will set the ID number to be a random number. I am doing this because I 
+        //want to minimize the chances of it being the same ID number that another team gave
+        Random rand = new Random();
+        int  n = rand.nextInt(5000);
+        return n;
+    }
+    public boolean isTeamOne(){
+        //need to pull our boolean the GreedTeam class
+        return myTeam.isTeamOne;
+    }
+}
---
+++ GreedTeam.java
@@ -0,0 +1,21 @@
+public class GreedTeam implements PlayerTeam{
+    public boolean isTeamOne;
+    public List<Robot> chooseTeam(boolean teamOne, GameState state){
+        this.isTeamOne = teamOne;
+        //making list to hold teams
+        List<Robots> theTeam = new List<Robots>();
+        //Now I am making 1 core bot to fill it and comment out other bots we want
+        CoreBot bot1 = new CoreBot();
+        //WolfBot bot 2 = new WolfBot();
+        //SkunkBot bot 3 = new SkunkBot();
+        //RabbitBot bot 4 = new RabbitBot();
+        //BatBot bot 5 = new BatBot();
+
+        //now add robots to list
+        theTeam.add(bot1);
+        return theTeam;
+    }
+    public List<Command> requestCommands(List<Location> information, List<Robot> robotsAwaitingCommand, GameState state){
+        //this needs to be made
+    }
+}
