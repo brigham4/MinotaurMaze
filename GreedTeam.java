a0e3cf96910592157438c9cecc20525f9cf52d09
6enevieve <evan.napper15@ncf.edu>
2017-11-09 14:44:53 -0500

    filled out requestCommands

--- GreedTeam.java
+++ GreedTeam.java
@@ -26,6 +26,38 @@ public class GreedTeam implements PlayerTeam{
     public List<Command> requestCommands(List<Location> information, List<Robot> robotsAwaitingCommand, GameState state){
         this.robotsAwaitingCommand = theTeam; 
         //all of our robots are awaiting commands
+        //this needs to be made
+        Location location1 = information.get(0);
+        Location location2 = information.get(1);
+        Location location3 = information.get(2);
+        Location location4 = information.get(3);
+        if (!location1.getCoins().isEmpty()){
+            CommandCoin command1 = new CommandCoin(robotsAwaitingCommand.get(0));
+        } else {
+            DirType dir1 = location1.getDirections.get(0);
+            CommandMove(robotsAwaitingCommand.get(0), dir1);
+        }
+
+        if (!location2.getCoins().isEmpty()){
+            CommandCoin command2 = new CommandCoin(robotsAwaitingCommand.get(1));
+        } else {
+            DirType dir2 = location2.getDirections.get(1);
+            CommandMove(robotsAwaitingCommand.get(1), dir2);
+        }
+
+        if (!location3.getCoins().isEmpty()){
+            CommandCoin command3 = new CommandCoin(robotsAwaitingCommand.get(2));
+        } else {
+            DirType dir3 = location3.getDirections.get(2);
+            CommandMove(robotsAwaitingCommand.get(2), dir3);
+        }
+
+        if (!location4.getCoins().isEmpty()){
+            CommandCoin command4 = new CommandCoin(robotsAwaitingCommand.get(3));
+        } else {
+            DirType dir4 = location4.getDirections.get(3);
+            CommandMove(robotsAwaitingCommand.get(3), dir4);
+        }
 
 
     }
