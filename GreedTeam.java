12c9a2a111a9fe4c540d5346029938f50fabb126
brigham4 <eric.brigham15@ncf.edu>
2017-11-09 14:23:52 -0500

    adding thing

--- GreedTeam.java
+++ GreedTeam.java
@@ -8,16 +8,25 @@ public class GreedTeam implements PlayerTeam{
         List<Robots> theTeam = new List<Robots>();
         //Now I am making 1 core bot to fill it and comment out other bots we want
         CoreBot bot1 = new CoreBot();
-        //WolfBot bot 2 = new WolfBot();
-        //SkunkBot bot 3 = new SkunkBot();
-        //RabbitBot bot 4 = new RabbitBot();
-        //BatBot bot 5 = new BatBot();
+        CoreBot bot2 = new CoreBot();
+        CoreBot bot3 = new CoreBot();
+        CoreBot bot4 = new CoreBot();
+        //WolfBot bot2 = new WolfBot();
+        //SkunkBot bot3 = new SkunkBot();
+        //RabbitBot bot4 = new RabbitBot();
+        //BatBot bot5 = new BatBot();
 
         //now add robots to list
         theTeam.add(bot1);
+        theTeam.add(bot2);
+        theTeam.add(bot3);
+        theTeam.add(bot4);
         return theTeam;
     }
     public List<Command> requestCommands(List<Location> information, List<Robot> robotsAwaitingCommand, GameState state){
-        //this needs to be made
+        this.robotsAwaitingCommand = theTeam; 
+        //all of our robots are awaiting commands
+
+
     }
 }
